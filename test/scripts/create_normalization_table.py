01ffb0430b68e9545660feb3f03f4a94ad2f76ec
Mario <mpel@protonmail.com>
2018-10-18 16:04:26 +0200

    cleanup of normalization table

--- test/scripts/create_normalization_table.py
+++ test/scripts/create_normalization_table.py
@@ -1,110 +1,46 @@
-import os
-
 ###All normalizations are provided to 1fb-1 of lumi in these tables
+import os
+import sys
 
+#xsec lookup in pb
+secs_table = dict()
+secs_table["ttbar"] = 831.76 - 87.31
+secs_table["ttbarlnu"] = 87.31
+secs_table["ttbarWQQ"] = 0.4062
+secs_table["ttbarWlnu"] = 0.2043
+secs_table["ttbarZQQ"] = 0.5297
+secs_table["ttbarZlnu"] = 0.2529
+secs_table["SingleTop_tW"] = 35.85
+secs_table["SingleAntiTop_tW"] = 35.85
+secs_table["WJetsToLNu"] = 20508.9*3.
+secs_table["DY_10_50"] = 18610.0
+secs_table["DY_50"] = 1921.8*3.
+secs_table["QCD_HT100to200"] = 27540000.0
+secs_table["QCD_HT200to300"] = 1717000.0
+secs_table["QCD_HT300to500"] = 351300.0
+secs_table["QCD_HT500to700"] = 31630.0
+secs_table["QCD_HT700to1000"] = 6802.0
+secs_table["QCD_HT1000to1500"] = 1206.0
+secs_table["QCD_HT1500to2000"] = 120.4
+secs_table["QCD_HT2000toInf"] = 25.25
+secs_table["ZZ"] = 8.16
+secs_table["WW"] = 51.723
+secs_table["WZ"] = 47.13
+secs_table["GammaJets_20_40"] = 219.2
+secs_table["GammaJets_40_Inf"] = 862.4
+secs_table["GammaJets_20_Inf"] = 3255.0
+secs_table["QCD_DoubleEMEnriched_30to40"] = 22180.0
+secs_table["QCD_DoubleEMEnriched_30toInf"] = 247000.0
+secs_table["QCD_DoubleEMEnriched_40toInf"] = 113100.0
+secs_table["TTGJets"] = 3.795
+secs_table["WGToLNuG"] = 489.0
+secs_table["ZGTo2LG"] = 117.864
+#cross section taken from https://arxiv.org/pdf/1611.04040.pdf, BR assumed 10-6, last factor 2 is because we have two possible final states (one for W+ and one for W-)
+secs_table["Signal"] = 831.76*0.1086*2.*0.000001*2.
 
-##These are in pb
-def get_xsec_fromsample(samplename):
-    
-    if samplename == "ttbar":
-        return 831.76 - 87.31
-
-    if samplename == "ttbarlnu":
-        return 87.31
-
-    if samplename == "ttbarWQQ":
-        return 0.4062
-
-    if "ttbarWlnu" in samplename:
-        return 0.2043
-                  
-    if samplename == "ttbarZQQ":
-        return 0.5297 
-
-    if "ttbarZlnu" in samplename:
-        return 0.2529 
-
-    if samplename == "SingleTop_tW":
-        return 35.85
-
-    if samplename == "SingleAntiTop_tW":
-        return 35.85
-
-    if samplename == "WJetsToLNu":
-        return 20508.9*3.
-
-    if "DY_10_50" in samplename:
-        return 18610.0
-
-    if "DY_50" in samplename:
-        return 1921.8*3.
-
-    if samplename == "QCD_HT100to200":
-        return 27540000.0 
-
-    if "QCD_HT200to300" in samplename:
-        return 1717000.0
-
-    if "QCD_HT300to500" in samplename:
-        return 351300.0
-
-    if "QCD_HT500to700" in samplename:
-        return 31630.0
-
-    if "QCD_HT700to1000" in samplename:
-        return 6802.0
-
-    if "QCD_HT1000to1500" in samplename:
-        return 1206.0
-
-    if "QCD_HT1500to2000" in samplename:
-        return 120.4 
-
-    if "QCD_HT2000toInf" in samplename:
-        return 25.25
-
-    if samplename == "ZZ":
-        return 8.16
-
-    if samplename == "WW":
-        return 51.723
-
-    if "WZ" in samplename:
-        return 47.13
-
-    if samplename == "GammaJets_20_40":
-        return 219.2
-
-    if samplename == "GammaJets_40_Inf":
-        return 862.4
-
-    if samplename == "GammaJets_20_Inf":
-        return 3255.0
-
-    if samplename == "QCD_DoubleEMEnriched_30to40":
-        return 22180.0
-
-    if samplename == "QCD_DoubleEMEnriched_30toInf":
-        return 247000.0
-
-    if samplename == "QCD_DoubleEMEnriched_40toInf":
-        return 113100.0
-
-    if "TTGJets" in samplename:
-        return 3.795
-    
-    if "WGToLNuG" in samplename:
-        return 489.
-
-    if "ZGTo2LG" in samplename:
-        return 117.864
-    
-    if "Signal" in samplename:
-        #cross section taken from https://arxiv.org/pdf/1611.04040.pdf, BR assumed 10-6, last factor 2 is because we have two possible final states (one for W+ and one for W-)
-        return 831.76*0.1086*2.*0.000001*2.
+complementary_samples_list = ["ttbarWlnu","ttbarZlnu","DY_10_50","DY_50","QCD_HT200to300","QCD_HT300to500","QCD_HT500to700","QCD_HT700to1000","QCD_HT1000to1500","QCD_HT1500to2000","QCD_HT2000toInf","WZ","WGToLNuG","TTGJets","ZGTo2LG","Signal"]
 
 ##Now starts the program
-
 def main():
 
     dir_input = "crab_projects/samples_Medium/"
@@ -112,24 +48,14 @@ def main():
 
     if not os.path.exists("rootfiles"):
         os.makedirs("rootfiles")
-        
 
     output_filename = "rootfiles/Medium/Normalizations_table.txt"
-
     out_file = open(output_filename,"w")
 
-
-    complementary_samples_list = ["ttbarWlnu","ttbarZlnu","DY_10_50","DY_50","QCD_HT200to300","QCD_HT300to500","QCD_HT500to700","QCD_HT700to1000","QCD_HT1000to1500","QCD_HT1500to2000","QCD_HT2000toInf","WZ","WGToLNuG","TTGJets","ZGTo2LG"]
-
     events_cumul = dict()
-
     for sample in complementary_samples_list:
         events_cumul[sample] = 0.
 
-    #Counter to treat signal differently: 2 samples, same xsec
-    signal_events_cumul = 0.
-
-    
     for dirname in list_dirs:
         
         samplename = dirname.split("crab_WPiGammaAnalysis_")[1]
@@ -143,107 +69,29 @@ def main():
         print "No. of events processed = " + str (number_events) + "\n"
         
         #Treat differently different samples with same xsec
+        for name_complement in complementary_samples_list:
+            if name_complement in samplename :
+                events_cumul[name_complement] = events_cumul[name_complement] + number_events
+                print "events_cumul: ", events_cumul[name_complement]
+                continue
 
-        if "ttbarWlnu" in samplename:
-            events_cumul["ttbarWlnu"] = events_cumul["ttbarWlnu"] + number_events
-            print "events_cumul: ", events_cumul["ttbarWlnu"]
-            continue
+        events_cumul[samplename] = number_events
 
-        if "ttbarZlnu" in samplename:
-            events_cumul["ttbarZlnu"] = events_cumul["ttbarZlnu"] + number_events
-            print "events_cumul: ", events_cumul["ttbarZlnu"]
-            continue
-
-        if "DY_10_50" in samplename:
-            events_cumul["DY_10_50"] = events_cumul["DY_10_50"] + number_events
-            print "events_cumul: ", events_cumul["DY_10_50"]
-            continue
-
-        if "DY_50" in samplename:
-            events_cumul["DY_50"] = events_cumul["DY_50"] + number_events
-            print "events_cumul: ", events_cumul["DY_50"]
-            continue
-
-        if "QCD_HT200to300" in samplename:
-            events_cumul["QCD_HT200to300"] = events_cumul["QCD_HT200to300"] + number_events
-            continue
-
-        if "QCD_HT300to500" in samplename:
-            events_cumul["QCD_HT300to500"] = events_cumul["QCD_HT300to500"] + number_events
-            continue
-
-        if "QCD_HT500to700" in samplename:
-            events_cumul["QCD_HT500to700"] = events_cumul["QCD_HT500to700"] + number_events
-            continue
-
-        if "QCD_HT700to1000" in samplename:
-            events_cumul["QCD_HT700to1000"] = events_cumul["QCD_HT700to1000"] + number_events
-            continue
-
-        if "QCD_HT1000to1500" in samplename:
-            events_cumul["QCD_HT1000to1500"] = events_cumul["QCD_HT1000to1500"] + number_events
-            continue
-
-        if "QCD_HT1500to2000" in samplename:
-            events_cumul["QCD_HT1500to2000"] = events_cumul["QCD_HT1500to2000"] + number_events
-            continue
-
-        if "QCD_HT2000toInf" in samplename:
-            events_cumul["QCD_HT2000toInf"] = events_cumul["QCD_HT2000toInf"] + number_events
-            continue
-
-        if "WZ" in samplename:
-            events_cumul["WZ"] = events_cumul["WZ"] + number_events
-            continue
-
-        if "WGToLNuG" in samplename:
-            events_cumul["WGToLNuG"] = events_cumul["WGToLNuG"] + number_events
-            continue
-
-        if "TTGJets" in samplename:
-            events_cumul["TTGJets"] = events_cumul["TTGJets"] + number_events
-            continue
-
-        if "ZGTo2LG" in samplename:
-            events_cumul["ZGTo2LG"] = events_cumul["ZGTo2LG"] + number_events
-            continue
-
-        #Treat signal differently to account for different samples with same xsec
-        if "Signal" in samplename:
-            signal_events_cumul = signal_events_cumul + number_events
-            continue
-
-
-        xsection = float(get_xsec_fromsample(samplename))
-        print "crossection = ", xsection
-        if number_events == 0:
+    for sample,event_count in events_cumul.iteritems():
+        if event_count == 0:
             scale_factor = 0.
             print "NUMBER OF EVENTS RETRIEVED = 0. SCALE FACTOR SET TO 0"
         else:
-            scale_factor = float(xsection*1000./number_events)
-            print "scale_factor = ", scale_factor
-            write_string = samplename + " " + str(scale_factor) + "\n"
-            print "Output Norm = ", write_string
-            out_file.write(write_string)
-            
-    if signal_events_cumul > 0.:
-        xsection = float(get_xsec_fromsample("Signal"))
-        scale_factor = float(xsection*1000./signal_events_cumul)
-        print "Signal scale factor = ", scale_factor
-        write_string = "Signal" + " " + str(scale_factor) + "\n"
-        print "Output Norm = ", write_string
-        out_file.write(write_string)
+            xsection = secs_table[sample]
+            print "Cross section = ", xsection
+            scale_factor = float(xsection*1000./events_cumul[sample])
+            print sample + " scale factor = ", scale_factor
 
-    for sample in complementary_samples_list:
-        xsection = float(get_xsec_fromsample(sample))
-        scale_factor = float(xsection*1000./events_cumul[sample])
-        print sample + " scale factor = ", scale_factor
         write_string = sample + " " + str(scale_factor) + "\n"
         print "Output Norm = ", write_string
         out_file.write(write_string)        
-
             
-    print "All done!"
+print "All done!"
 
 if __name__ == "__main__":
     main()
